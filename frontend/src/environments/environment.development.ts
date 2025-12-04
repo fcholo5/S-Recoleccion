4351c1d315b008f6b7a88c3e91eaec183c03c1bd
fcholo5 <panamenofabian29@gmail.com>
2025-12-04 17:55:03 -0500

    ya

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://srecoleccion.eleueleo.com/api', // Cambiado para usar local
+  serverUrl: 'http://srecoleccion.eleueleo.com/api/', // Cambiado para usar local
  apiUrl: 'http://apirecoleccion.gonzaloandreslucio.com/api'
   
 };
