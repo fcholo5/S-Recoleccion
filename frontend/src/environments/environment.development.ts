7f95396c7b8fdae60f4e01d82726c46db532a6e4
fcholo5 <panamenofabian29@gmail.com>
2025-12-04 16:38:35 -0500

    deploy full

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8000/api/', // Cambiado para usar local
+  serverUrl: 'http://srecoleccion.eleueleo.com/api/', // Cambiado para usar local
  apiUrl: 'http://apirecoleccion.gonzaloandreslucio.com/api'
   
 };
